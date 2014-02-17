5cf4a1de7955e88b0e7f9680fdbd3991585784c4
nsbarrcanvas <nick@canv.as>
2014-02-17 15:08:39 -0500

    Initial Commit

---
+++ FileSplitter/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FileSplitter
+//
+//  Created by Nicholas Barr on 2/17/14.
+//  Copyright (c) 2014 Nicholas Barr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
