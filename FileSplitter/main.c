abaffefbdcb66c2e68cf4acd7e4d28acedf0c64a
nsbarrcanvas <nick@canv.as>
2014-02-18 00:45:11 -0500

    sup bro

--- FileSplitter/main.c
+++ FileSplitter/main.c
@@ -7,12 +7,42 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, const char * argv[])
+
+#define MAXLINE 50
+
+int main(void)
 {
+    FILE *originalFile = fopen("foo/everyword.txt", "r");
+    FILE *outputFile = fopen("foo/lettersthatfollow_a.txt", "w");
+    char line [MAXLINE];
+    char currentFirstLetter = 'a';
+    char currentSecondLetter = '5';
+    
+    
+    while (fgets(line, MAXLINE, originalFile) != NULL && line[0] != '\n')
+        
+        if (line[0] == currentFirstLetter) {
+            if (line[1] != currentSecondLetter) {
+                fputc(line[1], outputFile);
+                currentSecondLetter = line[1];
+            }
+            
 
-    // insert code here...
-    printf("Hello, World!\n");
+        }
+        else {
+            currentSecondLetter = '5';
+            currentFirstLetter = line[0];
+            
+            char buffer[32];
+            snprintf(buffer, sizeof(char) * 32, "foo/lettersthatfollow_%c.txt", currentFirstLetter);
+            outputFile = fopen(buffer, "w");
+            if (line[1] != currentSecondLetter) {
+                fputc(line[1], outputFile);
+                currentSecondLetter = line[1];
+            }
+        }
     return 0;
 }
-
